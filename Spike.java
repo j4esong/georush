0bbea15064bc8cb629e3a676478e7e41b25cf630
Jeffrey Song <sevenkyus@gmail.com>
2022-01-25 20:56:33 -0800

    first working prototype

---
+++ Spike.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Spike extends Model {
+
+	public Vertex center;
+
+	public Spike(Vertex center, double size) {
+		super(new ArrayList<>());
+		this.center = center;
+		Vertex v1 = new Vertex(center.wx - 0.5 * size, center.wy - 0.5 * size, center.wz - 0.5 * size);
+        Vertex v2 = new Vertex(center.wx + 0.5 * size, center.wy - 0.5 * size, center.wz - 0.5 * size);
+        Vertex v3 = new Vertex(center.wx - 0.5 * size, center.wy - 0.5 * size, center.wz + 0.5 * size);
+        Vertex v4 = new Vertex(center.wx + 0.5 * size, center.wy - 0.5 * size, center.wz + 0.5 * size);
+        Vertex tip = new Vertex(center.wx, center.wy + 0.5 * size, center.wz);
+        lines.add(new Line(v1, v2));
+        lines.add(new Line(v2, v4));
+        lines.add(new Line(v4, v3));
+        lines.add(new Line(v3, v1));
+        lines.add(new Line(v1, tip));
+        lines.add(new Line(v2, tip));
+        lines.add(new Line(v3, tip));
+        lines.add(new Line(v4, tip));
+        color = color.RED;
+	}
+
+}
